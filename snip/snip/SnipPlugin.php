b000bd63cdfde0c1f35fcfd3a8452c2937cec214
Mark Notton <mark@marknotton.uk>
2016-11-10 00:31:25 +0000

    Settings text formatting updates

---
+++ snip/snip/SnipPlugin.php
@@ -0,0 +1,46 @@
+<?php
+namespace Craft;
+
+class SnipPlugin extends BasePlugin {
+  public function getName() {
+    return Craft::t('Snip');
+  }
+
+  public function getVersion() {
+    return '0.1';
+  }
+
+  public function getSchemaVersion() {
+    return '0.1';
+  }
+
+  public function getDescription() {
+    return 'Similar to Truncate, only with more features like a snippet filter with smart fallbacks.';
+  }
+
+  public function getDeveloper() {
+    return 'Yello Studio';
+  }
+
+  public function getDeveloperUrl() {
+    return 'http://yellostudio.co.uk';
+  }
+
+  public function getDocumentationUrl() {
+    return 'https://github.com/marknotton/craft-plugin-snip';
+  }
+
+  public function getReleaseFeedUrl() {
+    return 'https://raw.githubusercontent.com/marknotton/craft-plugin-snip/master/snip/releases.json';
+  }
+
+  public function addTwigExtension() {
+    Craft::import('plugins.snip.twigextensions.snip');
+    Craft::import('plugins.snip.twigextensions.snippet');
+    return array(
+      new snip(),
+      new snippet()
+    );
+  }
+
+}
